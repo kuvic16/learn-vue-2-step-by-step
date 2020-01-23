09fe83f659512ea8d800022d7638365fbef91791
Shaiful Islam <kuvic16@gmail.com>
2020-01-24 02:07:36 +0600

    component second part

--- js/e7_components.js
+++ js/e7_components.js
@@ -1,3 +1,21 @@
+Vue.component("task-list", {
+  template: `
+    <div>
+        <task v-for="task in tasks">{{task.task}}</task>
+    </div>
+    `,
+  data() {
+    return {
+      tasks: [
+        { task: "Learn, Learn & Learn", complete: true },
+        { task: "Sleep", complete: false },
+        { task: "Listen Music", complete: true },
+        { task: "Practise", complete: false }
+      ]
+    };
+  }
+});
+
 Vue.component("task", {
   template: `<li><slot></slot></li>`,
   data() {
