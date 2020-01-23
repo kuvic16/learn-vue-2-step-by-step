52f6f682ada3203d5aab68b7d76eb49ad95b5969
Shaiful Islam <kuvic16@gmail.com>
2020-01-24 01:37:39 +0600

    after few practise

---
+++ main.js
@@ -0,0 +1,32 @@
+window.Event = new (class {
+  constructor() {
+    this.vue = new Vue();
+  }
+
+  fire(event, data = null) {
+    this.vue.$emit(event, data);
+  }
+
+  listen(event, callback) {
+    this.vue.$on(event, callback);
+  }
+})();
+
+Vue.component("message", {
+  props: ["title", "body"],
+  template: `
+    <article class="message">
+        <div class="message-header">
+            <p>{{title}}</p>
+            <button class="delete" aria-label="delete"></button>
+        </div>
+        <div class="message-body">
+            {{body}}
+        </div>
+    </article>
+  `
+});
+
+new Vue({
+  el: "#root"
+});
