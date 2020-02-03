4828743a3e4ada8aa97c1e63174fa2ee13e1c358
Shaiful Islam <kuvic16@gmail.com>
2020-02-03 21:31:06 +0600

    episode 16 completed

---
+++ my-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <message>Hello there</message>
+    <message>Hi, How are you?</message>
+  </div>
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+export default {
+  name: "app",
+  components: { Message },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style></style>
