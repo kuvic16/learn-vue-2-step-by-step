b5e0a7e6f6fcc5342aa0e30a5f8b08e277d48129
Shaiful Islam <kuvic16@gmail.com>
2020-02-05 18:07:20 +0600

    episode 19 in progress

--- vue-app/public/js/app19.js
+++ vue-app/public/js/app19.js
@@ -1,14 +1,38 @@
+class Errors {
+    constructor() {
+        this.errors = {};
+    }
+
+    get(field) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
+
+    record(errors) {
+        this.errors = errors;
+    }
+
+    clear(field) {
+        delete this.errors[field];
+    }
+}
+
 new Vue({
     el: "#app",
     data() {
         return {
             name: "",
-            description: ""
+            description: "",
+            errors: new Errors()
         };
     },
     methods: {
         onSubmit() {
-            axios.post("/projects", this.$data);
+            axios
+                .post("/projects", this.$data)
+                .then(response => alert("Success"))
+                .catch(error => this.errors.record(error.response.data.errors));
         }
     }
 });
--- vue-app/resources/views/projects/create.blade.php
+++ vue-app/resources/views/projects/create.blade.php
@@ -14,11 +14,13 @@
         <form method="POST" action="/projects" @submit.prevent='onSubmit'>
             <div class="control">
                 <label for="name" class="label">Project Name:</label>
-                <input type="text" id="name" name="name" class="input" v-model="name">
+                <input type="text" id="name" name="name" class="input" v-model="name" @keydown="errors.clear('name')">
+                <span class="help is-danger" v-text="errors.get('name')"></span>
             </div>
             <div class="control">
                 <label for="description" class="label">Project description:</label>
-                <input type="text" id="description" name="description" class="input" v-model="description">
+                <input type="text" id="description" name="description" class="input" v-model="description" @keydown="errors.clear('description')">
+                <span class="help is-danger" v-text="errors.get('description')"></span>
             </div>
             <div class="control">
                 <button class="button is-primary">Create</button>
