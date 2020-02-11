d1ab5a8df0b76f8e07c661182b33a03b95813cfb
Shaiful Islam <kuvic16@gmail.com>
2020-02-11 15:29:52 +0600

    episode 24: shared state

--- vue-app/resources/views/welcome.blade.php
+++ vue-app/resources/views/welcome.blade.php
@@ -9,11 +9,24 @@
         
     </head>
     <body>
-        <div id="app">
+        {{-- <div id="app">
             <li v-for="skill in skills" v-text="skill"></li>
+        </div> --}}
+
+        <div id="one">
+            <h1>
+                @{{shared.user.name}}
+            </h1>
         </div>
+        <div id="two">
+            <h1>
+                @{{shared.user.name}}
+            </h1>
+        </div>
+
+
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        <script src="/js/app.js"></script>
+        <script src="/js/app24.js"></script>
     </body>
 </html>
