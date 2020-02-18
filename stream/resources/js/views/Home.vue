80254297f154b9b30422d8d985fcd26140702c89
Shaiful Islam <kuvic16@gmail.com>
2020-02-19 01:02:29 +0600

    episode 26 in progress

---
+++ stream/resources/js/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-heading">Home Page</div>
+          <div class="panel-body">I am an example component</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
