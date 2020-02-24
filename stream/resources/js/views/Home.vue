d54e2fe94be918dcfffd03d7745a5ac6ad777d8e
Shaiful Islam <kuvic16@gmail.com>
2020-02-25 02:09:14 +0600

    episode 27 completed

--- stream/resources/js/views/Home.vue
+++ stream/resources/js/views/Home.vue
@@ -2,16 +2,37 @@
     <div class="container">
         <div class="columns">
             <div class="column">
-                <div class="message">
-                    <div class="message-header">Home Page</div>
-                    <div class="message-body">I am an example component</div>
+                <div class="message" v-for="status in statuses">
+                    <div class="message-header">
+                        <p>{{ status.user.name }} said...</p>
+                        <p>{{ postedOn(status) }}</p>
+                    </div>
+                    <div class="message-body" v-text="status.body"></div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import moment from "moment";
+import Status from "../models/Status";
+
 export default {
+    data() {
+        return {
+            statuses: []
+        };
+    },
+    created() {
+        //axios.get("/statuses").then(({ data }) => (this.statuses = data));
+        //Status.all().then(({ data }) => (this.statuses = data));
+        Status.all(statuses => (this.statuses = statuses));
+    },
+    methods: {
+        postedOn(status) {
+            return moment(status.created_at).fromNow();
+        }
+    }
     // mounted() {
     //   console.log("Component mounted.");
     // }
