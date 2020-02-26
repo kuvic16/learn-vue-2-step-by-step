f5d4cef8f3240ed0f69dcef69e1b13c7e723a79d
Shaiful Islam <kuvic16@gmail.com>
2020-02-27 01:10:54 +0600

    filter completed

--- stream/resources/js/views/Home.vue
+++ stream/resources/js/views/Home.vue
@@ -1,41 +1,50 @@
 <template>
-    <div class="container">
-        <div class="columns">
-            <div class="column">
-                <div class="message" v-for="status in statuses">
-                    <div class="message-header">
-                        <p>{{ status.user.name }} said...</p>
-                        <p>{{ postedOn(status) }}</p>
-                    </div>
-                    <div class="message-body" v-text="status.body"></div>
-                </div>
-            </div>
+  <div class="container">
+    <div class="columns">
+      <div class="column">
+        <div class="message" v-for="status in statuses">
+          <div class="message-header">
+            <p>{{ status.user.name }} said...</p>
+            <!-- <p>{{ postedOn(status) }}</p> -->
+            <p>{{ status.created_at | ago | capitalize}}</p>
+          </div>
+          <div class="message-body" v-text="status.body"></div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import moment from "moment";
 import Status from "../models/Status";
 
 export default {
-    data() {
-        return {
-            statuses: []
-        };
+  data() {
+    return {
+      statuses: []
+    };
+  },
+  filters: {
+    ago(date) {
+      return moment(date).fromNow();
     },
-    created() {
-        //axios.get("/statuses").then(({ data }) => (this.statuses = data));
-        //Status.all().then(({ data }) => (this.statuses = data));
-        Status.all(statuses => (this.statuses = statuses));
-    },
-    methods: {
-        postedOn(status) {
-            return moment(status.created_at).fromNow();
-        }
+    capitalize(value) {
+      return value.toUpperCase();
     }
-    // mounted() {
-    //   console.log("Component mounted.");
-    // }
+  },
+  created() {
+    //axios.get("/statuses").then(({ data }) => (this.statuses = data));
+    //Status.all().then(({ data }) => (this.statuses = data));
+    Status.all(statuses => (this.statuses = statuses));
+  },
+  methods: {
+    postedOn(status) {
+      return moment(status.created_at).fromNow();
+    }
+  }
+  // mounted() {
+  //   console.log("Component mounted.");
+  // }
 };
 </script>
 <style></style>
