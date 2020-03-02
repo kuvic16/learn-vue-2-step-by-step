8b98111c7b7cfcead5fcf82b020a0c7d855aa1af
Shaiful Islam <kuvic16@gmail.com>
2020-03-02 11:18:09 +0600

    vue-testing episode completed

--- vue-testing/src/Notification.js
+++ vue-testing/src/Notification.js
@@ -1,7 +1,20 @@
 export default {
-  data() {
-    return {
-      message: "Hello World"
-    };
+  // template: "<div><h1>{{message | capitalize}}</h1></div>",
+  template: "<div><h1>{{notification}}</h1></div>",
+  props: ["message"],
+  filters: {
+    capitalize(message) {
+      return message.toUpperCase();
+    }
+  },
+  computed: {
+    notification() {
+      return this.message.toUpperCase();
+    }
   }
+  // data() {
+  //   return {
+  //     message: "Hello World"
+  //   };
+  // }
 };
--- vue-testing/test/Notification.js
+++ vue-testing/test/Notification.js
@@ -1,7 +1,37 @@
-import Vue from "vue";
+// import Vue from "vue";
+import Vue from "vue/dist/vue.js";
 import test from "ava";
 import Notification from "../src/Notification";
 
+let vm;
+test.beforeEach(t => {
+  let N = Vue.extend(Notification);
+  vm = new N({
+    propsData: {
+      message: "Foobar"
+    }
+  }).$mount();
+});
+
 test("that it renders a notification", t => {
-  t.is(Notification.data().message, "Hello World");
+  //t.is(Notification.data().message, "Hello World");
+  //let n = new Vue(Notification).$mount();
+  // console.log(n.$el.textContent);
+  // console.log(n.$el.innerHTML);
+
+  // let N = Vue.extend(Notification);
+  // let vm = new N({
+  //   propsData: {
+  //     message: "Foobar"
+  //   }
+  // }).$mount();
+  t.is(vm.$el.textContent, "FOOBAR");
+});
+
+test("that it capitalizes the message", t => {
+  t.is(vm.$el.textContent, "FOOBAR");
+});
+
+test("that it computes the notification", t => {
+  t.is(vm.notification, "FOOBAR");
 });
