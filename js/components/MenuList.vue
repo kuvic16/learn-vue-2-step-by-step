a231a9d5d37ca344738dc1112551fd83aa8f0e00
Shaiful Islam <kuvic16@gmail.com>
2020-03-19 11:39:32 +0600

    episode 33: completed

---
+++ js/components/MenuList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" v-text="item"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"]
+};
+</script>
