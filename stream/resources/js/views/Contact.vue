1f9f6f4e581ae29499a8339fced2ef258420f575
Shaiful Islam <kuvic16@gmail.com>
2020-03-22 16:42:52 +0600

    carousel episode completed

--- stream/resources/js/views/Contact.vue
+++ stream/resources/js/views/Contact.vue
@@ -4,14 +4,26 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Contact Page</div>
-                    <div class="panel-body">This is contact page</div>
+                    <div class="panel-body">
+                        This is contact page
+                        <carousel>
+                            <img src="https://placeimg.com/640/480/any" />
+                            <img src="https://placeimg.com/640/480/any?2" />
+                            <img src="https://placeimg.com/640/480/any?3" />
+                            <img src="https://placeimg.com/640/480/any?4" />
+                            <img src="https://placeimg.com/640/480/any?5" />
+                        </carousel>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import Carousel from "../components/Carousel";
+
 export default {
+    components: { Carousel }
     // mounted() {
     //   console.log("Component mounted.");
     // }
