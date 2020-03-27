5ee049f9aac0afeece7c2106dfd49447b070dcb9
Shaiful Islam <kuvic16@gmail.com>
2020-03-28 00:25:40 +0600

    tooltip episode completed

---
+++ stream/resources/js/components/Tooltip.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div ref="body" v-show="false">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import PopperTooltip from "tooltip.js";
+
+export default {
+    props: {
+        name: {},
+        placement: { default: "top" }
+    },
+    mounted() {
+        document
+            .querySelectorAll(`[data-tooltip-name=${this.name}]`)
+            .forEach(elem => {
+                new PopperTooltip(elem, {
+                    placement: this.placement,
+                    title: this.$refs.body.innerHTML,
+                    html: true
+                });
+            });
+    }
+};
+</script>
+
+<style lang="stylus"></style>
--- stream/resources/js/views/About.vue
+++ stream/resources/js/views/About.vue
@@ -1,49 +1,68 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-          <div class="panel-heading">About Page</div>
-          <div class="panel-body">This is about page</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">About Page</div>
+                    <div class="panel-body">This is about page</div>
 
-          <h5>Option#1</h5>
-          <p>
-            Hello There, Normal -
-            <span
-              data-tooltip="I am a tooltip"
-              data-tooltip-placement="right"
-            >hover me</span>
-          </p>
+                    <h5>Option#1</h5>
+                    <p>
+                        Hello There, Normal -
+                        <span
+                            data-tooltip="I am a tooltip"
+                            data-tooltip-placement="right"
+                            >hover me</span
+                        >
+                    </p>
 
-          <h5>Option#2</h5>
-          <p>
-            Hello There, Directive -
-            <span v-tooltip:top="'I am a tooltip'">hover me</span>
-          </p>
+                    <h5>Option#2</h5>
+                    <p>
+                        Hello There, Directive -
+                        <span v-tooltip:top="'I am a tooltip'">hover me</span>
+                    </p>
+
+                    <h5>Option#3</h5>
+                    <p>
+                        Hello There, Component -
+                        <span data-tooltip-name="our-products-tooltip"
+                            >hover me</span
+                        >
+                    </p>
+                    <tooltip name="our-products-tooltip" placement="left">
+                        <div class="p-4 w-64">
+                            <h1>Our Products</h1>
+                            <p>
+                                This is our first product. You can buy.
+                            </p>
+                        </div>
+                    </tooltip>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 <script>
-import Tooltip from "tooltip.js";
+import PopperTooltip from "tooltip.js";
+import Tooltip from "../components/Tooltip.vue";
 
 export default {
-  mounted() {
-    document.querySelectorAll("[data-tooltip]").forEach(elem => {
-      new Tooltip(elem, {
-        placement: elem.dataset.tooltipPlacement || "top",
-        title: elem.dataset.tooltip
-      });
-    });
-  }
+    components: { Tooltip },
+    mounted() {
+        document.querySelectorAll("[data-tooltip]").forEach(elem => {
+            new PopperTooltip(elem, {
+                placement: elem.dataset.tooltipPlacement || "top",
+                title: elem.dataset.tooltip
+            });
+        });
+    }
 };
 </script>
 <style>
 .tooltip {
-  background: black;
-  color: white;
-  padding: 10px;
-  border-radius: 5px;
+    background: black;
+    color: white;
+    padding: 10px;
+    border-radius: 5px;
 }
 </style>
