964bebba9d0f84a6fe2605ac5ce6655a389bbf1a
Shaiful Islam <kuvic16@gmail.com>
2020-03-29 01:18:05 +0600

    laravel-mix version

--- stream/resources/views/layouts/master.blade.php
+++ stream/resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{mix('/css/app.css')}}">
         <title>Stream App</title>        
     </head>
     <body>        
@@ -16,6 +16,6 @@
                 </div>
             </section>
         </div>
-        <script src="/js/app.js"></script>
+        <script src="{{mix('/js/app.js')}}"></script>
     </body>
 </html>
